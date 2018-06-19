894bbcfe00386048156ed93abe9e6fff36e4ea78
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-19 15:00:25 +0200

    Fixed typo in create person attr model migration.

--- db/migrate/20180619065235_create_personal_attributes.rb
+++ db/migrate/20180619065235_create_personal_attributes.rb
@@ -3,7 +3,7 @@ class CreatePersonalAttributes < ActiveRecord::Migration[5.2]
     create_table :personal_attributes do |t|
       t.string :value
       t.integer :person_id
-      t.integer :personal_attributes_type_id
+      t.integer :personal_attribute_type_id
       t.timestamps
     end
   end
