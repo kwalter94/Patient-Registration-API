f97dad5608edd22270f08b5dd4ff0c955079ca0e
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-19 18:50:07 +0200

    Added basic unit tests for person, role, and user models.
    CAUTION: Some of the tests are somewhat broken.

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can not create role with rolename" do
+    role = Role.new
+    assert_not role.save
+  end
+
+  test "Should create role with rolename" do
+    role = Role.new :rolename => 'foobar'
+    assert role.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not create user without username" do
+    assert_not new_user(without = [:username]).save
+  end
+
+  test "Should not create user without person" do
+    assert_not new_user(without = [:person]).save
+  end
+
+  test "Should not create user without password" do
+    assert_not new_user(without = [:password]).save
+  end
+
+  test "Should not create user without role" do
+    assert_not new_user(without = [:roles]).save
+  end
+
+  test "Should be able to create user with valid data" do
+    assert new_user.save
+  end
+
+  def new_user(without=[])
+    user_data = {
+      :username => 'Foobar',
+      :password => 'foobar', # This ought to be encrypted in the real world
+      :active => 'active',
+      :person => people(:kwalter),
+      :roles => [roles(:clerk)]
+    }
+
+    without.each {|field| user_data[field] = field == :roles and [] or nil}  # Unset all excluded fields
+
+    User.new(user_data)
+  end
 end
