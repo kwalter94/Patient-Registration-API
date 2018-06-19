0ffe499bb4b054b13428347e954a69f4e61b1e01
Martin Suleman <sulemartin87@gmail.com>
2018-06-19 09:45:08 +0200

    roles migartion

---
+++ db/migrate/20180619074353_create_user_roles.rb
@@ -0,0 +1,9 @@
+class CreateUserRoles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_roles do |t|
+      t.integer :role_id
+      t.integer :user_id 
+      t.timestamps
+    end
+  end
+end
