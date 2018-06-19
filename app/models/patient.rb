d21905a21671f3393df16d62ea6478d60407b11f
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-19 15:01:27 +0200

    Fixed syntax errors in model definitions

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < ApplicationRecord
     belongs_to :person
+    # validates :person_name, presense: true
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,3 @@
 class Role < ApplicationRecord
-    has_many :users, through :user_roles
+    has_many :users, :through => 'user_roles'
 end
