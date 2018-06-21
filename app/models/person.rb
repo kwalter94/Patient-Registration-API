652f8ac49c19743979318c06141c8d160918950f
Martin Suleman <sulemartin87@gmail.com>
2018-06-21 14:34:05 +0200

    Bug fix for missing field in person model.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,12 @@
 class Person < ApplicationRecord
     has_one :patient
     has_one :person
+    has_one :person_name
     has_many :personal_attributes
 
     # NOTE: Cases of people who do not know their date of birth
     # are encountered sometimes hence allowing Persons without
     # date of birth below...
-    validates :birthdate, presence: false 
+    validates :birthdate, presence: false
     validates :gender, presence: true
 end
