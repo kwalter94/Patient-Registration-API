d5fe0c03dafdd146561b9523e99696e25ef1bb1c
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-22 00:15:05 +0200

    Added roles and privileges routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'api#index'
 
-  resources :users do
-   member do
-     get :delete
-   end
- end
-
- resources :patients do
-  member do
-    get :delete
-  end
-end
+  resources :users
+  resources :patients
+  resources :roles
+  resources :privileges
 end
