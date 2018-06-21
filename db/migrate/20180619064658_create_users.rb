87f6bb6f7baa2a30336622d3d9ea3cbf90a2bc7e
Martin Suleman <sulemartin87@gmail.com>
2018-06-21 14:24:55 +0200

    Added default for user active field

--- db/migrate/20180619064658_create_users.rb
+++ db/migrate/20180619064658_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password 
-      t.boolean :active
+      t.string :password
+      t.boolean :active, :default => false 
       t.integer :person_id
       t.timestamps
     end
