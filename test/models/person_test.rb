03a50ee166b1fa767bff11034e52ed1b457307b2
Walter Kaunda <walterkaunda@live.co.uk>
2018-06-22 14:49:20 +0200

    Fixed person delete test

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -10,4 +10,18 @@ class PersonTest < ActiveSupport::TestCase
   test "Should not create person without gender" do
     assert_not Person.new(:gender => nil).save
   end
+
+  test "Should delete only if no user or patient is attached" do
+    # Can't destroy person with an attached user and/or client
+    person = people(:foobar)
+    assert_not(person.destroy(:patient))
+    assert_not(person.destroy(:user))
+    
+    # Can destroy person with no attached user and client
+    person = people(:dmr)   # dmr has no user attached
+    assert(person.destroy(:patient))
+
+    person = people(:rms)   # rms has no patient attached
+    assert(person.destroy(:user))
+  end
 end
