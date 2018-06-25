109288b83ce40d7e5fd232b6fa74618ba94a15ed
Martin Suleman <sulemartin87@gmail.com>
2018-06-25 09:43:40 +0200

    add deleted_at field for token

--- db/migrate/20180625063430_create_user_auths.rb
+++ db/migrate/20180625063430_create_user_auths.rb
@@ -3,6 +3,7 @@ class CreateUserAuths < ActiveRecord::Migration[5.2]
     create_table :user_auths do |t|
       t.integer :user_id
       t.string :token
+      t.datetime :deleted_at
       t.timestamps
     end
   end
